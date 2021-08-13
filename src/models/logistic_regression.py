b710bf63dceee3d426486580b13c13f042afa690
elisabethzinck <s164204@student.dtu.dk>
2021-08-13 10:36:38 +0200

    Create german_credit_pred.csv + clean up
    Save dataset with predictions st. only need to do predictions once.

--- src/models/logistic_regression.py
+++ src/models/logistic_regression.py
@@ -10,6 +10,7 @@ import os
 if __name__ == "__main__":
     print(os.getcwd())
     file_path = 'data\\processed\\german_credit.csv'
+    output_path = 'data\\processed\\german_credit_predictions.csv'
     data = pd.read_csv(file_path)
     data.head()
     n = data.shape[0]
@@ -17,15 +18,13 @@ if __name__ == "__main__":
     X = data[data.columns.difference(['credit_score'])]
     X = one_hot_encode_mixed_data(X)
 
-    # Generate random classifications
-    random.seed(60220)
-    preds = random.choices([0,1], k = n)
-    data['prediction'] = preds
-
     # Making logistic regression 
+    # To do: Research why logreg gives Convergence Warning
     log_reg = LogisticRegression(penalty='none')
     log_reg.fit(X, data.credit_score)
     data["logistic_regression_prediction"] = log_reg.predict(X)
+
+    data.to_csv(output_path, index = False)
 
     fair_log_reg = EvaluationTool(
         y = data.credit_score, 
@@ -38,10 +37,4 @@ if __name__ == "__main__":
     fair_log_reg.get_rates()
     fair_log_reg.separation()
 
-    fair_rand = EvaluationTool(
-        y = data.credit_score, 
-        c = data.prediction, 
-        a = data.sex, 
-        model_type='Random')
-    fair_rand.get_confusion_matrix()
-    fair_rand.plot_confusion_matrix()
+# %%
