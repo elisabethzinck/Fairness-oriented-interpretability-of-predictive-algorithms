418d5ee0308843218fd3804244b91753b22a7d0a
elisabethzinck <s164204@student.dtu.dk>
2021-08-17 11:43:51 +0200

    Update predictions on german credit
    Predictions now made with 10 fold cross validation + includes probabilites

--- src/models/logistic_regression.py
+++ src/models/logistic_regression.py
@@ -1,29 +1,43 @@
 #%%
 import pandas as pd
+import numpy as np
+
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import KFold
+
 from src.evaluation_tool.basic_tool import EvaluationTool 
 from src.data.general_preprocess_functions import one_hot_encode_mixed_data
-import random
-import os
+
 #%%
 
 if __name__ == "__main__":
-    print(os.getcwd())
     file_path = 'data\\processed\\german_credit.csv'
     output_path = 'data\\processed\\german_credit_pred.csv'
     data = pd.read_csv(file_path)
-    data.head()
-    n = data.shape[0]
+    
+    df = data.copy()
+    df['log_reg_pred'] = None
+    df['log_reg_prob'] = None
     
     X = data[data.columns.difference(['credit_score'])]
     X = one_hot_encode_mixed_data(X)
+    y = data.credit_score
 
-    # Making logistic regression 
-    # To do: Research why logreg gives Convergence Warning
-    log_reg = LogisticRegression(penalty='l2')
-    log_reg.fit(X, data.credit_score)
-    data["logistic_regression_prediction"] = log_reg.predict(X)
+    log_reg = LogisticRegression(
+        solver = 'liblinear', max_iter = 1000)
 
-    data.to_csv(output_path, index = False)
+        
+    k = 10 # Number of splits
+    kf = KFold(n_splits = k, random_state = 42, shuffle = True)
+
+    for train_idx, test_idx in kf.split(X):
+        X_train , X_test = X.iloc[train_idx,:], X.iloc[test_idx,:]
+        y_train , y_test = y[train_idx] , y[test_idx]
+        log_reg.fit(X_train, y_train)
+        df.loc[test_idx, 'log_reg_pred'] = log_reg.predict(X_test)
+        df.loc[test_idx, 'log_reg_prob'] = log_reg.predict_proba(X_test)[:,1] 
+    
+    
+    df.to_csv(output_path, index = False)
 
 # %%
