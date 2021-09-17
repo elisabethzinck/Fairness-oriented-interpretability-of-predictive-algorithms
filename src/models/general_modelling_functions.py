b011376fd39c8eca273479fab5eb866e8df194be
Caroline Fuglsang-Damgaard <36299120+CarolineFuglsang@users.noreply.github.com>
2021-09-17 11:10:29 +0200

    Adding myData, Net and BinaryClassificationTask to general modelling functions

--- src/models/general_modelling_functions.py
+++ src/models/general_modelling_functions.py
@@ -1,5 +1,20 @@
+#%% Imports
+from sklearn.metrics import accuracy_score
 
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset
 
+import pytorch_lightning as pl
+
+import warnings
+warnings.simplefilter("ignore")
+
+import logging
+logging.getLogger('lightning').setLevel(logging.ERROR)
+
+#%% Helper Functions
 def get_n_total_parameters(pytorch_model):
     n_params = sum(p.numel() for p in pytorch_model.parameters() if p.requires_grad)
     return n_params
@@ -13,44 +28,85 @@ def get_n_hidden_list(params):
     return n_hidden_list
 
 
-import pytorch_lightning as pl
-from pytorch_lightning.metrics import functional as FM
+#%% Define dataset
+class myData(Dataset):
+    def __init__(self, X_data, y_data):
+        self.X_data = torch.FloatTensor(X_data)
+        self.y_data = torch.FloatTensor(y_data[:, None]) # Make vector into matrix
+        
+    def __getitem__(self, index):
+        return self.X_data[index], self.y_data[index]
+        
+    def __len__ (self):
+        return len(self.X_data)
 
-# Soeren suggestion
-# From https://pytorch-lightning.readthedocs.io/en/latest/common/lightning_module.html#inference
-class ClassificationTask(pl.LightningModule):
-    def __init__(self, model):
+#%% Pytorch network
+class Net(nn.Module):
+    def __init__(self, num_features, num_hidden_list, num_output, p_dropout = 0):
+        super(Net, self).__init__()
+        n_hidden_layers = len(num_hidden_list)
+        self.layers = []
+        
+        input_dim = num_features
+        for i in range(n_hidden_layers):
+            output_dim = num_hidden_list[i]
+            self.layers.append(nn.Linear(input_dim, output_dim))
+            self.layers.append(nn.Dropout(p = p_dropout))
+            self.layers.append(nn.ReLU())
+            input_dim = output_dim
+        
+        # Last layer (without activation function)
+        self.layers.append(nn.Linear(num_hidden_list[-1], num_output))
+        self.layers.append(nn.Dropout(p = p_dropout))
+
+        self.layers = nn.Sequential(*self.layers)
+
+    def forward(self, x):
+        x = self.layers(x)
+        x = torch.sigmoid(x) 
+
+        return x
+
+#%% Pytorh Lightning network
+class BinaryClassificationTask(pl.LightningModule):
+    def __init__(self, model, lr = 1e-3):
         super().__init__()
         self.model = model
+        self.lr = lr
 
     def training_step(self, batch, batch_idx):
         x, y = batch
         y_hat = self.model(x)
-        loss = F.cross_entropy(y_hat, y)
+
+        loss, acc = self._shared_eval_step(batch, batch_idx)
+        metrics = {"train_loss": loss, 'train_acc': acc}
+        self.log_dict(metrics, on_epoch = True, on_step = False)
         return loss
 
     def validation_step(self, batch, batch_idx):
         loss, acc = self._shared_eval_step(batch, batch_idx)
-        metrics = {"val_acc": acc, "val_loss": loss}
+        metrics = {"val_loss": loss, 'val_acc': acc}
         self.log_dict(metrics)
         return metrics
 
     def test_step(self, batch, batch_idx):
         loss, acc = self._shared_eval_step(batch, batch_idx)
-        metrics = {"test_acc": acc, "test_loss": loss}
+        metrics = {"test_loss": loss, 'test_acc': acc}
         self.log_dict(metrics)
         return metrics
 
     def _shared_eval_step(self, batch, batch_idx):
         x, y = batch
         y_hat = self.model(x)
-        loss = F.cross_entropy(y_hat, y)
-        acc = FM.accuracy(y_hat, y)
+        loss = F.binary_cross_entropy(y_hat, y)
+        y_hat_binary = (y_hat >= 0.5)
+        acc = accuracy_score(y, y_hat_binary)
         return loss, acc
 
-    def predict_step(self, batch, batch_idx, dataloader_idx):
+    def predict_step(self, batch, batch_idx):
         x, y = batch
         y_hat = self.model(x)
+        return y_hat.to_numpy()
 
     def configure_optimizers(self):
-        return torch.optim.Adam(self.model.parameters(), lr=0.02)
+        return torch.optim.Adam(self.model.parameters(), lr = self.lr)
