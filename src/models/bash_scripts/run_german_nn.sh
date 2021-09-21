781618ac5f06845655a71e7bf7c2675c310a11a3
Elisabeth Zinck <s164204@student.dtu.dk>
2021-09-21 16:32:05 +0200

    First run with german done

---
+++ src/models/bash_scripts/run_german_nn.sh
@@ -0,0 +1,40 @@
+#!/bin/sh 
+### General options 
+### -- specify queue -- 
+#BSUB -q gpuv100
+### -- set the job Name -- 
+#BSUB -J german_neural_network
+### -- ask for number of cores (default: 1) -- 
+#BSUB -n 1 
+### --- ask for gpu ---
+#BSUB -gpu "num=1"
+### -- specify that the cores must be on the same host -- 
+#BSUB -R "span[hosts=1]"
+### -- amount of memory per core/slot -- 
+#BSUB -R "rusage[mem=5GB]"
+### -- job gets killed if it exceeds xGB per core/slot -- 
+#BSUB -M 5GB
+### -- set walltime limit: hh:mm -- 
+#BSUB -W 00:20 
+### -- send notification at completion -- 
+#BSUB -N 
+### -- Specify the output and error file. %J is the job-id -- 
+### -- -o and -e mean append, -oo and -eo mean overwrite -- 
+#BSUB -oo logs/german_neural_network.out 
+
+echo "Starting bash script"
+
+### Activate environment
+module load python3/3.8.11
+source .venv/bin/activate
+
+### Run python script
+echo "----------------------------"
+echo "--- Output from Python -----"
+echo "----------------------------"
+
+python3 src/models/german_neural_network.py
+
+echo "----------------------------"
+echo "---       DONE :)      -----"
+echo "----------------------------"
