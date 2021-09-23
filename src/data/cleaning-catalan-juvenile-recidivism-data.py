44201527f69975f6abbbcc23d4b84324e0b57217
Caroline Fuglsang-Damgaard <36299120+CarolineFuglsang@users.noreply.github.com>
2021-09-23 15:46:23 +0200

    catalan descriptive stuff and cleaning of vars

--- src/data/cleaning-catalan-juvenile-recidivism-data.py
+++ src/data/cleaning-catalan-juvenile-recidivism-data.py
@@ -51,27 +51,31 @@ df.dropna(axis = 0, subset=['V5_age_cat', 'V6_province', 'V7_region', 'V8_age'],
 #%% Filtering out columns, which are either specific MRM, ATM, Savry, 2013 or 2015 data
 dfsub = df.filter(list(df.columns[list(range(31))+[114]]))
 
-#%% Dropping the column V14_main_crime, as it is too specific 
-dfsub.drop(columns='V14_main_crime', inplace=True)
+#%% 
+# Dropping the column V14_main_crime, V7_region,
+# and V3_nationality_country as they are too specific 
+# Also dropping V5_age_cat, as we have the numeric variable 
+dfsub.drop(columns=['V3_nationality_country', 
+                    'V5_age_cat', 
+                    'V7_region',
+                    'V14_main_crime'], inplace=True)
 
 #%% Replacing some of the catalan words with english 
 V1_map = {'Dona': 'female', 'Home': 'male'}
 V2_map = {'Espanyol': 'Spanish', 'Estranger': 'Foreign'}
 V4_map = {'Espanya': 'Spain', 
-              'Magrib': 'Maghreb', 
-              'Centre i Sud Amèrica': 'Latin America',
-              'Altres': 'Other', 
-              'Europa': 'Europe'}
-V5_map = {'14 i 15 anys': '14-15 years', '16 i 17 anys':'16-17 years'}
+          'Magrib': 'Maghreb', 
+          'Centre i Sud Amèrica': 'Latin America',
+          'Altres': 'Other', 
+          'Europa': 'Europe'}
 V11_map = {'Amb antecedents': 1, 'Sense antecedents': 0}
 V12_map = {'1 o 2 antecedents': '1-2',
-                'De 3 a 5 antecedents': '3-5',
-                'Més de 5 antecedents': '5+',
-                0: '0'}
+           'De 3 a 5 antecedents': '3-5',
+           'Més de 5 antecedents': '5+',
+           0: '0'}
 V13_map = {'3 fets o més': '3+',
            '2 fets': '2',
-           '1 fet' : '1'
-}
+           '1 fet' : '1'}
 V15_map = {'Contra les persones': 'Against People',
             'Contra la propietat violent': 'Against Property',
             'Contra la propietat no violent': 'Against Property',
@@ -87,7 +91,6 @@ dfsub = dfsub.assign(
     V1_sex = lambda x: x.V1_sex.map(V1_map),
     V2_nationality_type = lambda x: x.V2_nationality_type.map(V2_map),
     V4_area_origin = lambda x: x.V4_area_origin.map(V4_map),
-    V5_age_cat = lambda x: x.V5_age_cat.map(V5_map),
     V8_age = lambda x: x.V8_age.astype(int),
     V11_criminal_record = lambda x: x.V11_criminal_record.map(V11_map),
     V12_n_criminal_record = lambda x: x.V12_n_criminal_record.map(V12_map),
--- src/visualization_description/catalan-descriptive-stuff.py
+++ src/visualization_description/catalan-descriptive-stuff.py
@@ -1,6 +1,5 @@
 #%%
 # imports 
-import matplotlib
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -8,33 +7,12 @@ import numpy as np
 
 import matplotlib.ticker as mtick
 import matplotlib.patches as mpatches
+from seaborn.palettes import color_palette
 
 from src.evaluation_tool.utils import custom_palette, abs_percentage_tick
 
 file_path = 'data\\processed\\catalan-juvenile-recidivism\\catalan-juvenile-recidivism-subset.xlsx'
 raw_data = pd.read_excel(file_path, index_col=0)
-
-
-# %% how many individuals are there from each country? 
-df1 = (raw_data.groupby(['V3_nationality_country'])
-            .agg(N_people = ('id', 'count'))
-            .reset_index()
-            )
-
-fig = plt.figure(figsize=(6,3))
-ax = fig.add_subplot(1, 1, 1)
-sns.boxplot(x = 'N_people', data = df1)
-
-# Grouping by N_people and adding a column with list of countries with that amount of N_people
-df2 = (df1.groupby(['N_people'])
-        .agg(N_countries = ('V3_nationality_country', 'count'))
-        .reset_index()
-        )
-countries = []
-for i in df2.N_people: 
-    countries.append(df1[df1['N_people'] == i].V3_nationality_country.to_list())
-df2['countries'] = countries
-
 
 #%% How do they distribute across V4_area_origin? 
 df_origin = (raw_data.groupby(['V4_area_origin'])
@@ -68,6 +46,40 @@ for pos in ['right', 'top', 'left']:
 ax.tick_params(left=False, labelsize=12)
 ax.set_title('#People by Area of Origin')
 plt.legend()
+
+#%% How do they distribute across V6_province? 
+df_province = (raw_data.groupby(['V6_province'])
+                .agg(N_people = ('id', 'count'), 
+                     recidivated = ('V115_RECID2015_recid', lambda x: np.count_nonzero(x)),
+                     not_recidivated = ('V115_RECID2015_recid', lambda x: len(x)-np.count_nonzero(x)),
+                     recid_frac = ('V115_RECID2015_recid', lambda x: np.count_nonzero(x)/len(x)))
+                .reset_index()
+                .sort_values(by = 'N_people', ascending = False)
+            )
+
+fig = plt.figure(figsize=(6,3))
+ax = fig.add_subplot(1, 1, 1)
+bar1 = sns.barplot(
+    x = 'N_people', y = 'V6_province', 
+    data = df_province,
+    estimator=sum,
+    palette = custom_palette(specific_col_idx = [6]),
+    label = 'Not Recidivated',
+    ax = ax)
+bar2 = sns.barplot(
+    x = 'recidivated', y = 'V6_province', 
+    data = df_province,
+    palette = custom_palette(specific_col_idx = [2]),
+    label ='Recidivated',
+    ax = ax)
+ax.set_xlabel('')
+ax.set_ylabel('')
+for pos in ['right', 'top', 'left']:
+    ax.spines[pos].set_visible(False)
+ax.tick_params(left=False, labelsize=12)
+ax.set_title('#People by Province')
+plt.legend()
+
 
 # %% How do they distribute across age?
 df_age = (raw_data.groupby(['V8_age'])
@@ -115,9 +127,9 @@ ax = fig.add_subplot(1, 1, 1)
 sns.boxplot(x = 'N_people', 
             y = 'V15_main_crime_cat',
             data = df_crime,
-            ax = ax)
+            ax = ax, 
+            palette = custom_palette(n_colors = 3))
 ax.set_ylabel('')
 ax.set_title('#People pr. Crime Category in V19_committed_crime \n Partitioned by Main Crime Category')
-
 
 # %%
