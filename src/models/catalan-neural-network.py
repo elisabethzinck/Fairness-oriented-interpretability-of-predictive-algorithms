64aeccfd2692277f23db20ec7879c80e78750c07
Caroline Fuglsang-Damgaard <36299120+CarolineFuglsang@users.noreply.github.com>
2021-09-23 16:17:29 +0200

    bash script for catalan neural network

--- src/models/catalan-neural-network.py
+++ src/models/catalan-neural-network.py
@@ -2,13 +2,14 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
 from sklearn.model_selection import train_test_split, KFold
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score
 
 from src.data.general_preprocess_functions import one_hot_encode_mixed_data
-from src.models.general_modelling_functions import (get_n_hidden_list, myData, Net, BinaryClassificationTask)
+from src.models.general_modelling_functions import (get_n_hidden_list, myData, Net, BinaryClassificationTask, print_timing)
 
 import torch
 from torch.utils.data import  DataLoader
@@ -25,6 +26,10 @@ warnings.simplefilter("ignore")
 
 import logging
 logging.getLogger('lightning').setLevel(logging.ERROR)
+
+#%% Epochs and trials 
+max_epochs = 100
+n_trials = 1000
 
 #%% Objective function for optuna optimizer
 def objective_function(trial: optuna.trial.Trial):
@@ -55,7 +60,7 @@ def objective_function(trial: optuna.trial.Trial):
     trainer = pl.Trainer(
         fast_dev_run = False,
         log_every_n_steps = 1, 
-        max_epochs = 100,
+        max_epochs = max_epochs,
         callbacks = [early_stopping, optuna_pruning], 
         deterministic = True,
         logger = False,
@@ -68,16 +73,20 @@ def objective_function(trial: optuna.trial.Trial):
 
 #%%
 if __name__ == "__main__":
+    t0 = time.time()
     pl.seed_everything(42)
 
     #Load data
-    file_path = 'data\\processed\\catalan-juvenile-recidivism\\catalan-juvenile-recidivism-subset.xlsx'
-    output_path = 'data\\predictions\\catalan-juvenile-recidivism\\catalan_recid_nn_pred.csv'
-    param_path = 'data\\predictions\\catalan-juvenile-recidivism\\catalan_recid_nn_pred_hyperparams.csv'
+    file_path = 'data/processed/catalan-juvenile-recidivism/catalan-juvenile-recidivism-subset.xlsx'
+    output_path = 'data/predictions/catalan-juvenile-recidivism/catalan_recid_nn_pred.csv'
+    param_path = 'data/predictions/catalan-juvenile-recidivism/catalan_recid_nn_pred_hyperparams.csv'
     raw_data = pd.read_excel(file_path, index_col=0)
-    
+
     assert raw_data.isnull().sum(axis = 0).max() == 0
-    
+
+    #Save models dir 
+    model_folder = 'models/catalan-juvenile-recidivism/'
+
     #Prepare data
     X = raw_data.drop(['V115_RECID2015_recid', 'id'], axis = 1)
     X = one_hot_encode_mixed_data(X)
@@ -121,7 +130,7 @@ if __name__ == "__main__":
         study.optimize(
             objective_function, 
             #timeout = max_minutes*60,
-            n_trials = 1000,
+            n_trials = n_trials,
             show_progress_bar=False)
 
 
@@ -147,13 +156,25 @@ if __name__ == "__main__":
         trainer = pl.Trainer(
             fast_dev_run = False,
             log_every_n_steps = 1, 
-            max_epochs = 100,
+            max_epochs = max_epochs,
             deterministic = True,
             callbacks = [early_stopping],
             progress_bar_refresh_rate = 0, 
             gpus = GPU)
 
         trainer.fit(plnet, train_loader, val_loader)
+
+        # Save model weigths, hparams and indexes for train and test data
+        checkpoint_file = f'{model_folder}NN_catalan_fold_{i}'
+        save_dict = {
+            'model': trainer.model,
+            'hparams': params,
+            'train_val_idx':train_val_idx, 
+            'test_idx': test_idx,
+            'fold': i}
+        torch.save(save_dict, checkpoint_file)
+
+
         #%%
         predictions = plnet.model.forward(torch.Tensor(X_test))
         pred_binary = (predictions >= 0.5)
@@ -162,8 +183,8 @@ if __name__ == "__main__":
         y_preds[test_idx] = predictions.detach().numpy().squeeze()
 # %%
     # Save data
-    potential_sensitive = ['V1_sex', 'V2_nationality_type', 'V3_nationality_country',
-                           'V4_area_origin', 'V5_age_cat', 'V6_province']
+    potential_sensitive = ['V1_sex', 'V2_nationality_type', 'V8_age',
+                           'V4_area_origin', 'V6_province']
     output_cols = ['id', 'V115_RECID2015_recid'] + potential_sensitive
     output_data = (raw_data[output_cols]
             .assign(
@@ -175,3 +196,6 @@ if __name__ == "__main__":
     params_df = pd.concat(
             [pd.DataFrame([paramdict], columns = paramdict.keys()) for paramdict in params_list])
     params_df.to_csv(param_path, index = False)
+
+    t1 = time.time()
+    print_timing(t0, t1, text = 'Total time to run script:')
