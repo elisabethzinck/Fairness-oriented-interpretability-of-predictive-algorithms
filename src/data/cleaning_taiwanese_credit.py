3df9e6d501cc42efde5d9a541f3b76c1467a67fd
elisabethzinck <s164204@student.dtu.dk>
2021-09-27 15:54:32 +0200

    Added Taiwanese Data (processed + raw)

---
+++ src/data/cleaning_taiwanese_credit.py
@@ -0,0 +1,54 @@
+#%%
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from src.data.general_preprocess_functions import map_several_columns
+
+#%%
+input_path = 'data/raw/taiwanese_credit/default of credit card clients.xls'
+output_path = 'data/processed/taiwanese_credit.csv'
+
+taiwan_credit_raw = pd.read_excel(input_path, skiprows=1)
+data = taiwan_credit_raw.copy() # To save downloading time
+
+
+# %% Renaming columns
+data.columns = [colname.lower() for colname in data.columns]
+data.rename(
+    columns = {'default payment next month': 'default_next_month'},
+    inplace = True)
+
+#%% Map categorical variables to strings
+# Variables originally encoded using numbers
+# education and marriage map don't correspond to explanation in article, 
+# instead map from  https://www.kaggle.com/uciml/default-of-credit-card-clients-dataset/discussion/34608 is used
+sex_map = {
+    1: 'Male',
+    2: 'Female'
+}
+education_map = {
+    0: 'others',
+    1: 'graduate school',
+    2: 'university',
+    3: 'high school',
+    4: 'others',
+    5: 'others',
+    6: 'others'
+    } 
+marriage_map = {
+    0: 'others',
+    1: 'married',
+    2: 'single',
+    3: 'others'} 
+
+map_dict = {'sex': sex_map, 'education': education_map, 'marriage': marriage_map}
+data = map_several_columns(data, map_dict)
+
+# %% Final checks before writing
+assert data.shape == (30000, 25)
+assert data.id.nunique() == 30000
+assert data.isnull().sum().sum() == 0
+
+# %% Write data
+data.to_csv(output_path)
+
--- src/data/general_preprocess_functions.py
+++ src/data/general_preprocess_functions.py
@@ -22,3 +22,27 @@ def one_hot_encode_mixed_data(X):
     X_final = pd.concat([X_num, X_cat_one_hot], axis = 1)
 
     return X_final 
+
+
+def map_several_columns(data, map_dict):
+    """Helper function for mapping several columns
+    
+    Args:
+        data (pd dataframe): Dataframe with data to be mapped
+        map_dict (dict of dictionaries): Dictionary containing dictionaries used for mapping. 
+        
+        Keys of dictionary must correspond to the names of the columns to be mapped in the dataframe. See cleaning_taiwanese_credit.py for example.
+        
+    """
+    columns_to_be_mapped = map_dict.keys()
+
+    for colname in columns_to_be_mapped:
+        null_before = data[colname].isnull().sum()
+        print(f'Mapping {colname}')
+        column_map = map_dict[colname]
+        data[colname] = data[colname].map(column_map)
+        null_after = data[colname].isnull().sum()
+        if null_before != null_after:
+            raise ValueError(f'Nulls where introduced when mapping {colname}')
+
+    return data
