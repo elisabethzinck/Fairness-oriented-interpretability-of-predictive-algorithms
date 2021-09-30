0ed3406f207092c1edc4f8de9e6cbddbc8e81bd7
Caroline Fuglsang-Damgaard <36299120+CarolineFuglsang@users.noreply.github.com>
2021-09-30 12:20:05 +0200

    Update describe_compas.py

--- src/visualization_description/describe_compas.py
+++ src/visualization_description/describe_compas.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np 
 import os
 
+from pandas.core.indexes.range import RangeIndex
+
 # reading data 
 df = pd.read_csv("data/processed/compas/compas-scores-two-years-pred.csv")
 
@@ -11,7 +13,7 @@ df = pd.read_csv("data/processed/compas/compas-scores-two-years-pred.csv")
 # Names and variables to be in table 
 sens_var = "race"
 target = "two_year_recid"
-target_tex_name = "Recidivated (%)"
+target_tex_name = "Recidivated"
 
 # Creating grouped table  
 N_pos_func = lambda x: np.count_nonzero(x)
@@ -23,34 +25,34 @@ df_grouped = (df.groupby([sens_var])
          N_positive = (target, N_pos_tab_func))
          .reset_index()
 )
-df_grouped.rename(columns = {target: sens_var.capitalize(), 
-                             "N_positive": target_tex_name})
+
+# appending total row: 
+row_total = pd.DataFrame({
+    sens_var: 'All', 
+    "N": len(df[target]),
+    "N_positive": N_pos_tab_func(df[target]),
+    }, index = [0])
+
+df_grouped =df_grouped.append(row_total, ignore_index=True)
+
+df_grouped.rename(columns = {sens_var: sens_var.capitalize(), 
+                             "N_positive": target_tex_name},
+                  inplace = True)
+
 # Styling the data frame 
-bold_cols = {'selector': 'th.col_heading',
-             'props': 'font-weight: bold;'}
+
 mid_rule = {'selector': 'midrule', 'props': ':hline;'}
-s = df_grouped.style.format()
-s.set_table_styles([bold_cols, mid_rule])
+s = df_grouped.style.format(escape = "latex")
+s.hide_index()
+s.set_table_styles([mid_rule])
 
 # printing style to Latex 
 column_format =  "l"+"c"*(df_grouped.shape[1]-1)
-s_tex = s.to_latex(column_format = column_format)
+s_tex = s.to_latex(column_format = column_format,
+                   convert_css = True)
 print(s_tex)
-
 
 # %%
 
-
-#%% 
-header = ["Race", "N", "Recidivated"]
-
-latex_table = df_grouped.to_latex(
-    header = header,
-    float_format = "%.0f",
-    column_format = "l"+"c"*(df_grouped.shape[1]-1), 
-    index = False,
-    )
-
-print(latex_table)
 
 
