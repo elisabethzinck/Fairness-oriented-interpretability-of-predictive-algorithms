86d998ad5617b08189267234b2059cb342b8f2aa
elisabethzinck <s164204@student.dtu.dk>
2021-11-15 10:42:06 +0100

    Update decribe_CheXpert.py

--- src/visualization_description/decribe_CheXpert.py
+++ src/visualization_description/decribe_CheXpert.py
@@ -11,20 +11,22 @@ from src.models.data_modules import CheXpertDataModule
 # Distributions without considering patients with more than one image
 
 #%% Aggregated tables for Cardiomelagy  
-dm = CheXpertDataModule(**{"target_disease": "Cardiomegaly"})
+disease = 'Cardiomegaly'
+dm = CheXpertDataModule(**{
+    "target_disease": disease})
 meta_dat = dm.train_data.dataset_df
 
 desc = DescribeData(a_name = "Sex", 
                     y_name = "y", 
                     id_name = 'patient_id', 
                     data = meta_dat,
-                    data_name='CheXpert, target: Cardiomegaly', 
+                    data_name=f'CheXpert, target: {disease}', 
                     **{"decimal":4})
 
-desc.descriptive_table_to_tex(target_tex_name='Has Cardiomegaly')
-desc.plot_positive_rate(title = 'Percentage with Cardiomegaly', orientation='v')
+desc.descriptive_table_to_tex(target_tex_name=f'Has {disease}')
+desc.plot_positive_rate(title = f'Percentage with {disease}', orientation='v')
 desc.plot_n_target_across_sens_var(
     orientation='v',
     return_ax=False, 
-    **{"class_1_label":"Cardiomegaly", "class_0_label": "No Cardiomegaly"})
-
+    **{"class_1_label":disease, "class_0_label": f"No {disease}"})
+# %%
