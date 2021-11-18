8eacdccefeaf75d851ae05b42b8f9a2c1e035dfe
Elisabeth Zinck <s164204@student.dtu.dk>
2021-11-18 14:57:55 +0100

    Succesfull 1 epoch run

--- src/models/bash_scripts/run_cheXpert_nn.sh
+++ src/models/bash_scripts/run_cheXpert_nn.sh
@@ -5,7 +5,7 @@
 ### -- set the job Name --  (should be the same as in script)
 #BSUB -J test_model
 ### -- ask for number of cores (default: 1) -- 
-#BSUB -n 1 
+#BSUB -n 8 
 ### --- ask for gpu ---
 #BSUB -gpu "num=1"
 ### -- specify that the cores must be on the same host -- 
