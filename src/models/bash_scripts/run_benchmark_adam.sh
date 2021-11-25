1889777cf9f1017428ff8d789f830980f207ab2f
Elisabeth Zinck <s164204@student.dtu.dk>
2021-11-25 13:39:04 +0100

    Final run Adam benchmark

--- src/models/bash_scripts/run_benchmark_adam.sh
+++ src/models/bash_scripts/run_benchmark_adam.sh
@@ -15,7 +15,7 @@
 ### -- job gets killed if it exceeds xGB per core/slot -- 
 #BSUB -M 10GB
 ### -- set walltime limit: hh:mm -- 
-#BSUB -W 02:30
+#BSUB -W 06:00
 ### -- Specify the output and error file. %J is the job-id -- 
 ### -- -o and -e mean append, -oo and -eo mean overwrite -- 
 #BSUB -oo logs/benchmark_adam/%J.out 
